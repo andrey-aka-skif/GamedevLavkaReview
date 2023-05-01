ef3dbb70c73c52d72beedb93344cfa11ba22b764
andrey-aka-skif <2119163@gmail.com>
2023-05-01 05:51:53 +0300

    Удалена версия x3

--- Assets/V01/Domain.Resources/Dto/ResourcesFeatureState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Assets.V01.Domain.Resources.Dto
-{
-    public class ResourcesFeatureState
-    {
-        public Dictionary<string, (object resource, int amount)> Resources { get; set; } = new Dictionary<string, (object resource, int amount)>();
-    }
-}
